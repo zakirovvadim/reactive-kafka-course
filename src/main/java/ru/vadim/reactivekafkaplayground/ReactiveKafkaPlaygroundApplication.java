d25047fa3e85882b0d57774495018c2ac9484f95
VadimZakirov <appolon17@gmail.com>
2025-02-09 12:44:33 +0500

    second initial commit

---
+++ src/main/java/ru/vadim/reactivekafkaplayground/ReactiveKafkaPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package ru.vadim.reactivekafkaplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveKafkaPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactiveKafkaPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vadim/reactivekafkaplayground/ReactiveKafkaPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vadim.reactivekafkaplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactiveKafkaPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
