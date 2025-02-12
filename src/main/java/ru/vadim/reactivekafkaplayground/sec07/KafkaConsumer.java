3998a55e64e5a3bac61b11e47a68a5f5a901026a
VadimZakirov <appolon17@gmail.com>
2025-02-12 22:49:41 +0500

    Kafka cluster example classes (секция 8 можно посмотреть только с запускм в докере 3 брокеров, докер компоуз в проекте кафка воркспейс 03)

--- src/main/java/ru/vadim/reactivekafkaplayground/sec07/KafkaConsumer.java
+++ src/main/java/ru/vadim/reactivekafkaplayground/sec07/KafkaConsumer.java
@@ -17,31 +17,53 @@ public class KafkaConsumer {
 
     public static final Logger log = LoggerFactory.getLogger(KafkaConsumer.class);
 
-    public static void main(String[] args) {
-        Map<String, Object> consumerConfig = Map.of(
-                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
-                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
-                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
-                ConsumerConfig.GROUP_ID_CONFIG, "demo-group-123",
-                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
-                ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1"
-        );
-        ReceiverOptions<Object, Object> options = ReceiverOptions.create(consumerConfig)
-                .addAssignListener(c -> {
-                    c.forEach(p -> log.info("assigned: {}", p.position()));
-//                    c.forEach(r -> r.seek(r.position() - 2)); // можно получать сообщения не с саомго первого,а с двигом оффсета на 2, число не должно быть отрицательным
-                    c.stream()
-                            .filter(r -> r.topicPartition().partition() == 2)//1. ищет партицию с конкретным номером `2`.
-                            .findFirst()
-                            .ifPresent(r -> r.seek(r.position() - 2)); // 1. ищет партицию с конкретным номером `2`. Помимо числоа можно указывать время
-                })
-                .subscription(List.of("order-events"));
+//    public static void main(String[] args) {
+//        Map<String, Object> consumerConfig = Map.of(
+//                ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092",
+//                ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+//                ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class,
+//                ConsumerConfig.GROUP_ID_CONFIG, "demo-group-123",
+//                ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest",
+//                ConsumerConfig.GROUP_INSTANCE_ID_CONFIG, "1"
+//        );
+//        ReceiverOptions<Object, Object> options = ReceiverOptions.create(consumerConfig)
+//                .addAssignListener(c -> {
+//                    c.forEach(p -> log.info("assigned: {}", p.position()));
+////                    c.forEach(r -> r.seek(r.position() - 2)); // можно получать сообщения не с саомго первого,а с двигом оффсета на 2, число не должно быть отрицательным
+//                    c.stream()
+//                            .filter(r -> r.topicPartition().partition() == 2)//1. ищет партицию с конкретным номером `2`.
+//                            .findFirst()
+//                            .ifPresent(r -> r.seek(r.position() - 2)); // 1. ищет партицию с конкретным номером `2`. Помимо числоа можно указывать время
+//                })
+//                .subscription(List.of("order-events"));
+//
+//        KafkaReceiver.create(options)
+//                .receive()
+//                .doOnNext(r -> log.info("key: {}, value: {}", r.key(), r.value()))
+//                .doOnNext(r -> r.receiverOffset().acknowledge())
+//                .subscribe();
+//
+//    }
 
-        KafkaReceiver.create(options)
-                .receive()
-                .doOnNext(r -> log.info("key: {}, value: {}", r.key(), r.value()))
-                .doOnNext(r -> r.receiverOffset().acknowledge())
-                .subscribe();
+    public static void main(String[] args) {
+        System.out.println(mergeAlternately("abc", "pqr"));
+    }
+    /**
+     * Merges two strings alternately, character by character. If one string is longer than the other,
+     **/
+    public static String mergeAlternately(String word1, String word2) {
+        StringBuilder builder = new StringBuilder();
+        if (word1.length() > word2.length()) {
+            for (int i = 0; i < word2.length(); i++) {
+                if (i == word2.length() - 1) {
+                    builder.append(word1.substring(i));
+                } else {
+                    builder.append(word1.charAt(i));
+                    builder.append(word2.charAt(i));
+                }
+            }
+        }
+        return builder.toString();
     }
 
 }
