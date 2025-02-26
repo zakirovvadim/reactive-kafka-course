97b5b2dd5e7b666c947d7d700479b00b0a06d799
VadimZakirov <appolon17@gmail.com>
2025-02-26 20:17:22 +0500

    Start sec 10 Reactive kafka with spring lec 119

---
+++ src/main/java/ru/vadim/reactivekafkaplayground/sec16/KafkaConsumerConfig.java
@@ -0,0 +1,24 @@
+package ru.vadim.reactivekafkaplayground.sec16;
+
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.core.reactive.ReactiveKafkaConsumerTemplate;
+import reactor.kafka.receiver.ReceiverOptions;
+
+import java.util.List;
+
+@Configurable
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ReceiverOptions<String, String> receiverOptions(KafkaProperties kafkaProperties) {
+        return ReceiverOptions.<String, String>create(kafkaProperties.buildConsumerProperties())
+                .subscription(List.of("order-events"));
+    }
+
+    @Bean
+    public ReactiveKafkaConsumerTemplate<String, String> consumerTemplate(ReceiverOptions<String, String> receiverOptions) {
+        return new ReactiveKafkaConsumerTemplate<>(receiverOptions);
+    }
+}
